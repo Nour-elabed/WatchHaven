faf8b26520bac54f5d1a98477e60736d403d544f
Nour1717 <elabed.nour@gmail.com>
2026-03-04 15:41:04 +0100

    login page

--- src/App.tsx
+++ src/App.tsx
@@ -5,40 +5,45 @@ import Product from './_root/pages/Product'
 import './index.css'
 import RootLayout from './_root/pages/RootLayout'
 import Login from './_root/pages/Login'
-import { useEffect, useState } from 'react'
 import Register from './_root/pages/Register'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
+
+type User = {
+  _id: string
+  username: string
+  email: string
+  token: string
+}
+
 const App = () => {
-  const [user, setUser] = useState(null)
-  const [error, setError] = useState("")
+  const [user, setUser] = useState<User | null>(null)
+
   useEffect(() => {
     const fetchUser = async () => {
-      const token= localStorage.getItem("token")
-      if(token){
+      const token = localStorage.getItem("token")
+      if (token) {
         try {
-          const response = await axios("/api/users/profile", {
-            headers: {
-              Authorization: `Bearer ${token}`
-            }
+          const response = await axios.get("/api/users/profile", {
+            headers: { Authorization: `Bearer ${token}` }
           })
           setUser(response.data)
-         } catch(err) {
-          setError("Failed to fetch user data")
+        } catch {
           localStorage.removeItem("token")
         }
       }
     }
     fetchUser()
   }, [])
+
   return (
     <Routes>
       <Route element={<RootLayout />}>
         <Route index element={<Home />} />
         <Route path='/shop' element={<Shop />} />
         <Route path='/product' element={<Product />} />
-        <Route path='/login' element={<Login setUser={setUser}/>}  />
-                <Route path='/register' element={<Register setUser={setUser}/>}  />
-
+        <Route path='/login' element={<Login setUser={setUser} />} />
+        <Route path='/register' element={<Register  />} />
       </Route>
     </Routes>
   )
