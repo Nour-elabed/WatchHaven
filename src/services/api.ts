017b3578eeac60cf5845a450dffddd5f97598c4f
Nour1717 <elabed.nour@gmail.com>
2026-04-13 18:17:22 +0100

    updates

--- src/services/api.ts
+++ src/services/api.ts
@@ -10,7 +10,7 @@
  */
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "/api";
+const BASE_URL = import.meta.env.VITE_API_URL ;
 
 const api = axios.create({
     baseURL: BASE_URL,
